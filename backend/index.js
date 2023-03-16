70cef42fada157334c244a3f5184159335de64e8
jugnugs <55590442+jugnugs@users.noreply.github.com>
2023-03-16 13:37:43 -0400

    express web server setup

--- backend/index.js
+++ backend/index.js
@@ -2,25 +2,33 @@ const oracledb = require('oracledb');
 const dbConfig = require('./dbconfig.js');
 
 try {
-    oracledb.initOracleClient({libDir: process.env.ORACLE_CLIENT_DIR});
+  oracledb.initOracleClient({libDir: process.env.ORACLE_CLIENT_DIR});
 } catch (err) {
-    console.error('could not find oracle client');
-    console.error(err);
-    process.exit(1);
+  console.error('could not find oracle client');
+  console.error(err);
+  process.exit(1);
 }
 
-async function run() {
+const express = require('express');
+const app = express();
+const port = 5000;
 
+app.listen(port, () => console.log("Backend Express API listening on port %s!", port));
+
+app.get('/', (req, res) => {
+  test(req, res);
+});
+
+async function test(req, res) {
   let connection;
-
+  let data;
   try {
     connection = await oracledb.getConnection(dbConfig);
     console.log("Successfully connected to Oracle Database!");
 
-    const data = await connection.execute(
+    data = await connection.execute(
         "select table_name from user_tables"
     );
-    console.log(data.rows);
 
   } catch (err) {
     console.error(err);
@@ -32,7 +40,7 @@ async function run() {
         console.error(err);
       }
     }
+    res.send(data.rows);
   }
 }
 
-run();
