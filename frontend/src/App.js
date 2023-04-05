fce34979159327625a093cad09e7a621c0cd7614
jugnugs <55590442+jugnugs@users.noreply.github.com>
2023-04-04 21:36:44 -0400

    country select menu & map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import * as iso_countries from 'i18n-iso-countries';
+
 import './App.css';
 import World from './components/World';
 import Header from './components/Header';
 import Menu from './components/Menu';
 import theme from './theme';
 
+iso_countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
 
 function App() {
-  const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState({});
+
+  const updateCountries = (key) => {
+    console.log(key);
+    if (key in countries) {
+      let newCountries = {
+        ...countries
+      };
+      delete newCountries[key];
+      setCountries(newCountries);
+    } else {
+      let newCountry = { [key]: iso_countries.getName(key, 'en') }
+      setCountries(countries => ({
+        ...countries,
+        ...newCountry
+      }));
+    }
+  };
 
   return (
     <ChakraProvider theme={theme}>
       <div className="App">
         <Header />
-        <Menu 
-          selectedCountries={countries} 
-          onSelect={(e) => setCountries(['USA'])} 
+        <Menu
+          selectedCountries={countries}
+          onSelect={updateCountries}
         />
-        <World 
-          selectedCountries={countries} 
-          onSelect={(e) => setCountries(['USA'])}
+        <World
+          selectedCountries={countries}
+          onSelect={updateCountries}
         />
       </div>
     </ChakraProvider>
--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,19 +1,32 @@
 import { Box, Button, Checkbox, Container, Heading, Stack } from "@chakra-ui/react";
-import React from "react"
+import React, { useEffect, useState } from "react"
+import * as iso_countries from 'i18n-iso-countries';
 
 const Menu = ({ selectedCountries, onSelect }) => {
+    const [countries, setCountries] = useState({});
+
+    useEffect(() => {
+        setCountries(iso_countries.getNames('en'));
+        console.log(countries);
+    }, []);
+
     return (
         <Container pos='fixed' bg='white' h='100%' w='sm' paddingTop='100px' zIndex='docked' borderRight='1px' borderColor='gray.200'>
-            <Stack spacing={5}>
-                <Box>
-                    <Heading>Country</Heading>
-                    <Checkbox
-                        onChange={onSelect}
-                        isChecked={selectedCountries.includes('USA')}
-                    >
-                        USA
-                    </Checkbox>
-                </Box>
+            <Stack spacing={5} overflowY='auto' h='100%'>
+                <Heading variant='menu-heading'>Country</Heading>
+                <Stack h='50%' overflowY='auto'>
+                        {
+                            Object.keys(countries).map((key) => {
+                                return <Checkbox
+                                    onChange={(e) => onSelect(key)}
+                                    isChecked={key in selectedCountries}
+                                    key={key}
+                                >
+                                    {countries[key]}
+                                </Checkbox>
+                            })
+                        }
+                </Stack>
                 <Button>Submit</Button>
             </Stack>
         </Container>
