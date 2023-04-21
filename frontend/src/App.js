a71c3f7631cfa37076cf471e0b00101096ebceb9
chos906 <80611197+chos906@users.noreply.github.com>
2023-04-20 20:30:39 -0400

    App.js Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,79 @@
-import { useState } from 'react';
-import { Box, ChakraProvider, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Stack } from '@chakra-ui/react';
-import * as iso_countries from 'i18n-iso-countries';
-import axios from 'axios';
+import { useState } from "react";
+import {
+  Box,
+  ChakraProvider,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+} from "@chakra-ui/react";
+import * as iso_countries from "i18n-iso-countries";
+import axios from "axios";
 
-import './App.css';
-import World from './components/World';
-import Header from './components/Header';
-import Menu from './components/Menu';
-import theme from './theme';
-import Graph from './components/Graph';
+import "./App.css";
+import World from "./components/World";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import theme from "./theme";
+import Graph from "./components/Graph";
 
 iso_countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
 
 const baseURL = "http://localhost:5000/";
 
+const countryList = {
+  AM: "Armenia",
+  AZ: "Azerbaijan",
+  EG: "Egypt",
+  GT: "Guatemala",
+  PR: "Peru",
+  RW: "Rwanda",
+  TJ: "Tajikistan",
+  TR: "Turkey",
+  UG: "Uganda",
+  UA: "Ukraine",
+  ZM: "Zambia",
+  ZW: "Zimbabwe",
+};
+
 function App() {
   const [countries, setCountries] = useState({});
   const [displayGraph, setDisplayGraph] = useState(false);
+  const [yearRange, setYearRange] = useState([1990, 2020]);
+  const [foodItems, setFoodItems] = useState([]);
 
   const updateCountries = (key) => {
     console.log(key);
     if (key in countries) {
       let newCountries = {
-        ...countries
+        ...countries,
       };
       delete newCountries[key];
       setCountries(newCountries);
     } else {
-      let newCountry = { [key]: iso_countries.getName(key, 'en') }
-      setCountries(countries => ({
+      let newCountry = { [key]: iso_countries.getName(key, "en") };
+      setCountries((countries) => ({
         ...countries,
-        ...newCountry
+        ...newCountry,
       }));
+    }
+  };
+
+  const updateYears = (val) => {
+    setYearRange(val);
+  };
+
+  const updateFoodItems = (val) => {
+    const i = foodItems.indexOf(val);
+    let array = [...foodItems];
+    if (i === -1) {
+      setFoodItems([...array, val]);
+    } else {
+      array.splice(i, 1);
+      setFoodItems(array);
     }
   };
 
@@ -39,24 +81,24 @@ function App() {
     // GET request to different URL depending on query
     axios.get(`${baseURL}inflationRate`).then((res) => {
       console.log(res.data);
-    })
-  }
+    });
+  };
 
   const handleSubmit = () => {
     getData();
     setDisplayGraph(true);
-  }
+  };
 
   const onClose = () => {
     setDisplayGraph(false);
-  }
+  };
 
-  const renderCountryList = (
-    Object.keys(countries).map((key, i) => (
-      i === Object.keys(countries).length - 1 ?
-        <span>{countries[key]}</span> :
-        <span>{countries[key]}, </span> 
-    ))
+  const renderCountryList = Object.keys(countries).map((key, i) =>
+    i === Object.keys(countries).length - 1 ? (
+      <span>{countries[key]}</span>
+    ) : (
+      <span>{countries[key]}, </span>
+    )
   );
 
   return (
@@ -64,8 +106,13 @@ function App() {
       <div className="App">
         <Header />
         <Menu
+          countryList={countryList}
           selectedCountries={countries}
-          onSelect={updateCountries}
+          selectedYears={yearRange}
+          selectedFoodItems={foodItems}
+          onSelectCountry={updateCountries}
+          onSelectYears={updateYears}
+          onSelectFoodItems={updateFoodItems}
           onSubmit={handleSubmit}
         />
         <Modal isOpen={displayGraph} onClose={onClose} isCentered>
@@ -75,15 +122,14 @@ function App() {
             <ModalCloseButton />
             <ModalBody>
               <Stack>
-                <Box>
-                  Selected Countries: {renderCountryList}
-                </Box>
+                <Box>Selected Countries: {renderCountryList}</Box>
               </Stack>
               <Graph></Graph>
             </ModalBody>
           </ModalContent>
         </Modal>
         <World
+          countryList={countryList}
           selectedCountries={countries}
           onSelect={updateCountries}
         />
